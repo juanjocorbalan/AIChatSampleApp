0006c43f89d76927721ee263779a5ec5c9b02dcd
Juanjo Corbalán <juanjocorbalan@gmail.com>
2025-04-08 15:53:46 +0200

    Initial Commit

---
+++ AIChatSampleApp/AIChatSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatSampleAppApp.swift
+//  AIChatSampleApp
+//
+//  Created by Juanjo Corbalan on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
